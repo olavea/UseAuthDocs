0d24f4a78fed862b8f9345e7e141ac941ce4e031
Ola Vea <ola@olavea.com>
2020-01-09 08:26:37 +0100

    home page text commited

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,8 +16,10 @@ const Header = styled(Box)`
 `
 
 const Container = styled(Box)`
-  max-width: 720px;
-  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 
 class Layout extends React.Component {
@@ -26,24 +28,41 @@ class Layout extends React.Component {
     return (
       <ThemeProvider theme={theme}>
         <React.Fragment>
-          <Header py={5} mb={5}>
-            <Heading fontSize={[6, 7]}>Coolest Library</Heading>
-            <Heading fontWeight={400}>Some cool subtitle here!</Heading>
+          <Header>
             <Box display="flex" mt={4}>
-              <Button variant="primary" onClick={() => navigate('/')}>
-                Home
-              </Button>
-              <Button
-                ml={2}
-                variant="primary"
-                onClick={() => navigate('/docs')}
-              >
-                Getting Started
+              <Heading fontSize={[6, 7]}>
+                React app authentication added
+              </Heading>
+              <Heading fontWeight={400}>
+                You lean back and useAuth handles users, login forms, redirects,
+                sharing state between components. Everything.
+              </Heading>
+            </Box>
+            <Box display="flex" mt={4}>
+              <Button variant="primary" onClick={() => navigate('/docs')}>
+                GET STARTED
               </Button>
             </Box>
           </Header>
           <Container>
-            <main>{children}</main>
+            <main>
+              <Box display="flex" mt={4}>
+                <Heading fontWeight={400}>Enjoy useAuth</Heading>
+                <Heading fontWeight={400}>
+                  You can even say hello to your users
+                </Heading>
+                <p>...</p>
+              </Box>
+              <Box display="flex" mt={4}>
+                <Button
+                  ml={2}
+                  variant="primary"
+                  onClick={() => navigate('/docs')}
+                >
+                  Getting Started
+                </Button>
+              </Box>
+            </main>
             <hr
               style={{
                 marginTop: rhythm(1),
